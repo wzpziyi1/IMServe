4e58fa4893df8b3337944fb052c08c9b3450b59f
wzpziyi1 <1204473090@qq.com>
2017-05-16 15:48:41 +0800

    Initial Commit

---
+++ Serve/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Serve
+//
+//  Created by 王志盼 on 16/05/2017.
+//  Copyright © 2017 王志盼. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
